9395ae5de2c58555d5493d74c786df73f230b223
DeFri1337 <148345441+DeFri1337@users.noreply.github.com>
2023-11-16 14:05:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import pygame
 
+import time
+
+timer = 1 * 60
+
+while timer > 0:
+    mins, secs = divmod(timer, 60)
+    timer_format = '{:02d}:{:02d}'.format(mins, secs)
+    print(timer_format, end="\r")
+    time.sleep(1)
+    timer -= 1
+
+print("Таймер завершен!")
+
